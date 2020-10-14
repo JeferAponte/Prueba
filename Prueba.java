deaebab774cee280609338c83c09cbb32368ad81
Jeferson <jsalazara@ucundinamarca.edu.co>
2020-10-14 14:20:45 -0500

    Primer Commit

---
+++ Prueba.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Prueba
+{ 
+	public static void main (String [] args) 
+	{
+        String nombre = "Jeferson Salazar Aponte";
+		System.out.println("");
+		System.out.println("Hola Mundo, Bienvenido: "+nombre);
+    }
+}
